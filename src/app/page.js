aff5d6d98255aabe60c1c729e8497f9e15abf0a3
devrazec <devrazec@gmail.com>
2025-12-10 15:58:00 +0100

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,10 @@
-import DataGenerator from '../utils/DataGenerator';
-
 import Header from '../components/Header';
 
 import Content from '../components/Content';
 
-import Footer from '../components/Footer';
-
 export default function Home() {
   return (
     <>
-      {/* <DataGenerator /> */}
       <Header />
       <Content />
     </>
